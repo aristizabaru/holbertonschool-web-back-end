dd99787d1eab47cba9af20e528045a897770d479
Andrés Aristizábal <andres.aristizabal@gmail.com>
2022-01-31 22:14:52 +0000

    first commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""add two floats and return their sum"""
+
+
+def add(a: float, b: float) -> float:
+    """add two floats and return their sum
+
+    Args:
+        a (float): first number
+        b (float): second number
+
+    Returns:
+        float: sum of a and b
+    """
+    return a + b
