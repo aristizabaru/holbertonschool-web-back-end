e10ec61d435d1c774d357b47a9eb7e9decc67f69
Andrés Aristizábal <andres.aristizabal@gmail.com>
2022-01-31 22:21:40 +0000

    first commit

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""takes a string 'str1' and a string 'str2' as arguments
+and returns a concatenated string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenate two strings"""
+    return str1 + str2
