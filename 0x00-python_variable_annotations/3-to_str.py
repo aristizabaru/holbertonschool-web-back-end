2b8f8a6202db56c8b4df0289264c4ac297f94348
Andrés Aristizábal <andres.aristizabal@gmail.com>
2022-01-31 22:39:12 +0000

    doc: add docstring to function

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -4,4 +4,13 @@ representation of the float"""
 
 
 def to_str(n: float) -> str:
+    """takes a float n as argument and returns the string
+representation of the float
+
+    Args:
+        n (float): float number
+
+    Returns:
+        str: string representation of 'n'
+    """
     return str(n)
