c4f08b7b1ea3593e466bc885b05a520f2b00ed7a
Andrés Aristizábal <andres.aristizabal@gmail.com>
2022-02-02 01:50:29 +0000

    fix: add max_delay default to 10

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,7 +8,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int,  max_delay: int) -> List[float]:
+async def task_wait_n(n: int,  max_delay: int = 10) -> List[float]:
     """create 'n' tasks and return results in order of resolution
 
     Args:
