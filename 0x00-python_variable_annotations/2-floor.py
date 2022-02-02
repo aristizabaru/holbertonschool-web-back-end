c10c1001e1ba06850d5fb514407aa76050c51e23
Andrés Aristizábal <andres.aristizabal@gmail.com>
2022-02-02 09:47:40 +0000

    first commit

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""2-floor"""
+import math
+
+
+def floor(n: float) -> int:
+    """takes a float n as argument and returns
+    the floor of the float.
+
+    Args:
+        n (float): argument to be return as int
+
+    Returns:
+        int: result from floor operation
+    """
+    return math.floor(n)
