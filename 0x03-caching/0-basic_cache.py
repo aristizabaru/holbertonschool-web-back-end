dbd964b35d58ebf58b3bf9785d9cf05f4a7f3afe
Andrés Aristizábal <andres.aristizabal@gmail.com>
2022-02-06 13:28:08 +0000

    doc: add doctring to BasicCache methods

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -12,11 +12,13 @@ class BasicCache(BaseCaching):
     """
 
     def put(self, key, item):
+        """ Add an item in the cache """
         if(key is None or item is None):
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """ Get an item by key """
         if(key is None or key not in self.cache_data.keys()):
             return
         return self.cache_data.get(key)
