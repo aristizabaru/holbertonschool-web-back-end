6b495d8c5e83edf742a67661bdd8c166d1703212
Andrés Aristizábal <andres.aristizabal@gmail.com>
2022-02-06 16:14:04 +0000

    first commit

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""2-lifo_cache"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """creates a LRU cache system without limit
+
+   methods:
+        put(key, item): adds a key/value pair to cache
+        get(key): gets the key value from cache
+    """
+
+    def __init__(self):
+        """init object"""
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if(key is None or item is None):
+            return
+        if(len(self.cache_data.keys()) == BaseCaching.MAX_ITEMS
+           and key not in self.cache_data.keys()):
+            discard_key = list(self.cache_data.keys())[0]
+            del self.cache_data[discard_key]
+            print("DISCARD: {}".format(discard_key))
+        if(key in self.cache_data.keys()):
+            del self.cache_data[key]
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key """
+        if(key is None or key not in self.cache_data.keys()):
+            return
+        item = self.cache_data[key]
+        self.put(key, item)
+        return self.cache_data.get(key)
