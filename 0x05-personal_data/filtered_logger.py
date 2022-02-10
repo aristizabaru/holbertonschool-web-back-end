7727a1cfab173fe14333d12d68a7307613e7f7a3
Andrés Aristizábal <andres.aristizabal@gmail.com>
2022-02-10 22:56:25 +0000

    first commit

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python3
 """filtered_logger module"""
 
+import logging
 from typing import List
 import re
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        # create format simple format object with custom
+        # attribute fields and method filter_datum()
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+
+        # super().format(record) returns string formated
+        # to then call filter_datum() to obfuscate it
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record),
+                            self.SEPARATOR)
 
 
 def filter_datum(fields: List[str],
@@ -23,3 +49,21 @@ def filter_datum(fields: List[str],
                          '{}={}{}'.format(field, redaction, separator),
                          message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """returns a new logger
+
+    Returns:
+        logging.Logger: new logger with RedactingFormatter() format
+    """
+    LOGGER_NAME = "user_data"
+
+    new_logger = logging.getLogger(LOGGER_NAME)
+    new_logger.setLevel(logging.INFO)
+    formatter = RedactingFormatter(PII_FIELDS)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    new_logger.addHandler(stream_handler)
+
+    return new_logger
