daca035be335b6be1753a31a183326761bb64542
Andrés Aristizábal <andres.aristizabal@gmail.com>
2022-02-11 21:08:20 +0000

    first commit

---
+++ 0x05-personal_data/encrypt_password.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """turn password string to salted, hashed password byte string
+
+    Args:
+        password (str): password
+
+    Returns:
+        bytes: hashed password
+    """
+    byte_password = password.encode('utf-8')
+    hashed = bcrypt.hashpw(byte_password, bcrypt.gensalt())
+
+    return hashed
