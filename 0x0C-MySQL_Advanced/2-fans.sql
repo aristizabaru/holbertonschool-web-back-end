efa31808743c9c286ddc6624f25bb64e91f763b1
aristizabaru <andres.aristizabal@gmail.com>
2022-03-17 20:26:14 +0000

    fix: using wrong query for the file, change the query

--- 0x0C-MySQL_Advanced/2-fans.sql
+++ 0x0C-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,4 @@
 --SQL script that ranks country origins of bands
-SELECT DISTINCT `band_name`,
-                IFNULL(`split`, 2020) - `formed` as `lifespan`
-  FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
-  ORDER BY `lifespan` DESC;
+SELECT DISTINCT `origin`, SUM(`fans`) as `nb_fans` FROM `metal_bands`
+GROUP BY `origin`
+ORDER BY `nb_fans` DESC;
