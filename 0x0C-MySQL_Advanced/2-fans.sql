83ffde09945f165b6099f3d5838fc27cfdaffff0
aristizabaru <andres.aristizabal@gmail.com>
2022-03-17 20:25:10 +0000

    fix: tabs

--- 0x0C-MySQL_Advanced/2-fans.sql
+++ 0x0C-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 --SQL script that ranks country origins of bands
 SELECT DISTINCT `band_name`,
                 IFNULL(`split`, 2020) - `formed` as `lifespan`
-    FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
-    ORDER BY `lifespan` DESC;
+  FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
+  ORDER BY `lifespan` DESC;
