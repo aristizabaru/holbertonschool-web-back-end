db76354758ede14fcab4aef0c4ae710948ad5e35
aristizabaru <andres.aristizabal@gmail.com>
2022-03-17 20:42:38 +0000

    fix: change comment sintax

--- 0x0C-MySQL_Advanced/4-store.sql
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -1,5 +1,5 @@
---SQL script that creates a trigger that decreases the quantity
---of an item after adding a new order.
+-- SQL script that creates a trigger that decreases the quantity
+-- of an item after adding a new order.
 CREATE TRIGGER order_decrease BEFORE INSERT ON orders
 FOR EACH ROW UPDATE items
 SET quantity = quantity - NEW.number
