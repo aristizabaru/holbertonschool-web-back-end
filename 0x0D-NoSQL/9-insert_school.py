9eac85449830750e76eeeead7b913ef2bb36255b
aristizabaru <andres.aristizabal@gmail.com>
2022-03-23 02:38:14 +0000

    first commit

---
+++ 0x0D-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" 9-insert_school module
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """  inserts a new document in a collection based on kwargs
+    """
+    document_id = mongo_collection.insert(kwargs)
+    return document_id
