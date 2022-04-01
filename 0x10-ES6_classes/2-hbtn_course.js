ae7bb5690e8612325ca37959b6458ded7a91c4cc
aristizabaru <andres.aristizabal@gmail.com>
2022-04-01 02:59:16 +0000

    fix: check variable in set length method

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -1,7 +1,13 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
     this._name = name;
+
+    if (typeof length !== 'number') throw new TypeError('Length must be a number');
     this._length = length;
+
+    if (!Array.isArray(students) || students.every((s) => typeof s !== 'string'))
+      throw new TypeError('Students must be a array of strings');
     this._students = students;
   }
 
@@ -19,7 +25,7 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
-    if (!typeof length !== 'number') throw new TypeError('Length must be a number');
+    if (typeof length !== 'number') throw new TypeError('Length must be a number');
     this._length = length;
   }
 
