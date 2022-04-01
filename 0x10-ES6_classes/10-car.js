da67c5180e854bf75110bd89ba05bf49341f1473
aristizabaru <andres.aristizabal@gmail.com>
2022-04-01 03:55:48 +0000

    fix: remove guards on constructor

--- 0x10-ES6_classes/10-car.js
+++ 0x10-ES6_classes/10-car.js
@@ -1,10 +1,7 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') throw new TypeError('brand must be a string');
     this._brand = brand;
-    if (typeof motor !== 'string') throw new TypeError('motor must be a string');
     this._motor = motor;
-    if (typeof color !== 'string') throw new TypeError('color must be a string');
     this._color = color;
   }
 
@@ -14,6 +11,6 @@ export default class Car {
 
   cloneCar() {
     const Clone = this.constructor[Symbol.species];
-    return new Clone(this._brand, this._motor, this._color);
+    return new Clone();
   }
 }
