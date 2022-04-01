d1aed2a13ea62cccab18805b53f6e3b0356eb0f3
aristizabaru <andres.aristizabal@gmail.com>
2022-04-01 03:36:18 +0000

    first commit

---
+++ 0x10-ES6_classes/7-airport.js
@@ -0,0 +1,12 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') throw new TypeError('name must be a string');
+    this._name = name;
+    if (typeof code !== 'string') throw new TypeError('code must be a string');
+    this._code = code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
