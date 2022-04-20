0ead4f1f320ec0acad7d9b7f357020e599930a75
aristizabaru <andres.aristizabal@gmail.com>
2022-04-20 02:50:01 +0000

    feat: add log to succesful connection

--- 0x14-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x14-queuing_system_in_js/4-redis_advanced_op.js
@@ -3,6 +3,15 @@ import redis from 'redis';
 const cli = redis.createClient();
 const keys = ['Portland', 'Seattle', 'New York', 'Bogota', 'Cali', 'Paris'];
 const val = [50, 80, 20, 20, 40, 2];
+
+cli.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+cli.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
 keys.forEach((key, index) => {
   cli.hset('HolbertonSchools', key, val[index], redis.print);
 });
